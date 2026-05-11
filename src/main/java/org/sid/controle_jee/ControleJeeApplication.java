2270cd83f5c1eca4777b95802465302a4029bd0c
soukaina-boop <soukainaaitali03@gmail.com>
2026-05-11 15:21:02 +0100

    Création projet Spring Boot et dépendances

---
+++ src/main/java/org/sid/controle_jee/ControleJeeApplication.java
@@ -0,0 +1,13 @@
+package org.sid.controle_jee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/controle_jee/ControleJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.controle_jee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
